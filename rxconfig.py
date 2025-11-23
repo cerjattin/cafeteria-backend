d5bb6163586b6cec2ec272bf87f2a72a1e997907
Edgardo Cervantes Jattin <cerjattin@gmail.com>
2025-11-23 12:54:08 -0500

    Config: Autorizar URL de Vercel en CORS

--- rxconfig.py
+++ rxconfig.py
@@ -1,16 +1,21 @@
 import reflex as rx
+import os
+
 BACKEND_URL = "https://cafeteria-backend-mfg6.onrender.com"
 
-config = rx.Config(
-    app_name="FavoredCoffee",
-    api_url=BACKEND_URL,
-    deploy_url= "https://FavoredCoffee.reflex.run",
+# Pega aquí tu URL de Vercel exacta que copiaste
+FRONTEND_URL = "https://favored-coffee.vercel.app" 
 
+config = rx.Config(
+    app_name="cafeteria_app", 
+    api_url=BACKEND_URL,
+    deploy_url=BACKEND_URL, 
+    
     cors_allowed_origins=[
         "http://localhost:3000",
         BACKEND_URL,
-        "https://*.vercel.app",
+        FRONTEND_URL,
+      
+
     ],
-    # Deshabilitar plugins innecesarios
-    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
