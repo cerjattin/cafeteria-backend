bdbc35ed2d9710c91a6ecd3d00e7126ab3ac5ca3
Edgardo Cervantes Jattin <cerjattin@gmail.com>
2025-11-24 10:54:34 -0500

    Cambios en Rxconfig direccion vercel

--- rxconfig.py
+++ rxconfig.py
@@ -4,4 +4,9 @@ config = rx.Config(
     app_name="cafeteria_app",
     api_url="https://cafeteria-backend-mfg6.onrender.com",
     
+    cors_allowed_origins=[
+        "http://localhost:3000",  # <--- IMPORTANTE: Permite tu entorno local
+        "https://cafeteria-backend-mfg6.onrender.com",
+        "https://*.vercel.app"    # Déjalo por si luego volvemos a Vercel
+    ],
 )
