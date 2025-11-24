3f09ce5ce24dbbff62ae0a645a28f45fb66a85c5
Edgardo Cervantes Jattin <cerjattin@gmail.com>
2025-11-24 10:19:04 -0500

    Cambios en Rxconfig libre

--- rxconfig.py
+++ rxconfig.py
@@ -1,20 +1,11 @@
 import reflex as rx
 
-# Tu Backend (Render)
-BACKEND_URL = "https://cafeteria-backend-mfg6.onrender.com"
-
-# Tu Frontend (Vercel) - ¡ESTA ES LA QUE OBTUVIMOS DEL LOG!
-FRONTEND_URL = "https://favored-coffee.vercel.app" 
-
 config = rx.Config(
-    app_name="cafeteria_app", 
+    app_name="cafeteria_app",
+    api_url="https://cafeteria-backend-mfg6.onrender.com",
     
-    api_url=BACKEND_URL,
-    
+    # 👇 CAMBIA ESTO. Permite entrar a cualquiera.
     cors_allowed_origins=[
-        "http://localhost:3000",
-        BACKEND_URL,
-        
-        FRONTEND_URL, 
+        "*"
     ],
-)s
+)
