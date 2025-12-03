cd8e92b02634f91ea17e85466eee5c8ca4ca0110
Edgardo Cervantes Jattin <cerjattin@gmail.com>
2025-12-03 17:38:23 -0500

    ajustes al main conectar con frontnd local

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ app = FastAPI(title=settings.APP_NAME)
 
 origins = [ 
     "https://*.vercel.app",
-    "http://localhost:3000",]
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",]
 
 # CORS para React/Vercel
 app.add_middleware(
